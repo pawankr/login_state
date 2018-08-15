8e237227c7eadeb9686f5e598a9e54597f4f4e0d
Pawan Dontula <Pawan.Dontula@emc.com>
2018-08-15 16:05:00 +0100

    empty commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import '../lib/src/app.dart';
-import 'package:login_stateful/main.dart';
+// import 'package:login_stateful/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
